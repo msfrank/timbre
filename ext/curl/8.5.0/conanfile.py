a80adbe62cde7ec70856e8e2339531df7795b1fb
Michael Frank <msfrank@syntaxjockey.com>
2024-05-12 19:47:34 -0700

    set correct curl cmake directory
    * add missing timbre user on curl 8.5.0 recipe

--- ext/curl/8.5.0/conanfile.py
+++ ext/curl/8.5.0/conanfile.py
@@ -11,6 +11,7 @@ CURL_SOURCE_DIR = "curl-8.5.0"
 class Curl(ConanFile):
     name = "curl"
     version = "8.5.0"
+    user = "timbre"
     url = "https://curl.se"
     description = """
 Command line tool and library for transferring data with URLs (since 1998).
@@ -31,44 +32,40 @@ Command line tool and library for transferring data with URLs (since 1998).
     def generate(self):
         tc = CMakeToolchain(self)
 
-        #tc.variables['CARES_ROOT'] = self.dependencies['cares'].package_folder
+        # ensure library name is the same for release and debug builds
+        tc.variables['CMAKE_DEBUG_POSTFIX'] = ''
 
-        # curl has a custom find script that searches for ares.h. setting CMAKE_PREFIX_PATH helps the find_*
-        # commands detect the cares headers and libraries.
+        # curl has a custom find script that searches for ares.h. setting CMAKE_PREFIX_PATH helps
+        # the find_* commands detect the cares headers and libraries.
         tc.variables['CMAKE_PREFIX_PATH'] = self.dependencies['cares'].cpp_info.includedirs
 
-        # curl uses the default cmake FindOpenSSL script, which takes OPENSSL_ROOT_DIR as a hint to locate
-        # the openssl headers and libraries.
+        # curl uses the default cmake FindOpenSSL script, which takes OPENSSL_ROOT_DIR as a hint
+        # to locate the openssl headers and libraries.
         tc.variables['OPENSSL_ROOT_DIR'] = self.dependencies['openssl'].package_folder
 
         tc.cache_variables['BUILD_CURL_EXE'] = 'OFF'
         tc.cache_variables['BUILD_TESTING'] = 'OFF'
         tc.cache_variables['CURL_ENABLE_SSL'] = 'ON'
-        tc.cache_variables['ENABLE_ARES'] = 'ON'
-        tc.cache_variables['ENABLE_DEBUG'] = 'ON'
-        tc.cache_variables['HTTP_ONLY'] = 'ON'
         tc.cache_variables['CURL_USE_OPENSSL'] = 'ON'
+        tc.cache_variables['ENABLE_ARES'] = 'ON'
         tc.cache_variables['CMAKE_INSTALL_RPATH_USE_LINK_PATH'] = 'ON'
 
         tc.generate()
         deps = CMakeDeps(self)
         deps.generate()
 
-
     def build(self):
         cmake = CMake(self)
-        cmake.verbose = True
         cmake.configure()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        copy(self, '*.so', self.build_folder, join(self.package_folder,'lib'), keep_path=False)
-        copy(self, '*.dylib', self.build_folder, join(self.package_folder,'lib'), keep_path=False)
-        copy(self, '*.a', self.build_folder, join(self.package_folder,'lib'), keep_path=False)
-        copy(self, '*.h', self.build_folder, join(self.package_folder,'include'))
+        cmake = CMake(self)
+        cmake.install()
+        # copy license file to share/ package directory
         copy(self, 'COPYING', self.build_folder, join(self.package_folder,'share'))
 
     def package_info(self):
-        self.cpp_info.libs = ['curl']
+        self.cpp_info.set_property("cmake_find_mode", "none")
+        self.cpp_info.builddirs.append(join("lib", "cmake", "CURL"))
         self.conf_info.define('user.curl.license_file', join(self.package_folder, 'share', 'COPYING'))
